e651e813e93697d0729bec62df7dd64bc2c85b33
EmillySilva1 <pereiraemillysilva@gmail.com>
2023-05-18 11:03:44 -0300

    entendi pouca coisa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Comentário
+        // Aula introdutória de Orientação á Objetos
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome ="lee";
+        System.out.println(qualquer.falar("oi"));
     }
 }
